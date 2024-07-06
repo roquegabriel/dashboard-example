6bbce65efd4b67ccc28d24b456b2375da411947c
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-06 00:32:31 -0400

    Add header section

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className='flex justify-between px-4 pt-4'>
+            <h2>Dashboard</h2>
+            <h2>Welcome Back, Roque</h2>
+        </div>
+    )
+}
